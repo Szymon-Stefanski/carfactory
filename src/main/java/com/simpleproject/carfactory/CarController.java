b43a9e042be9cc6a278ef7351e586cec3ca475ff
Szymon-Stefanski <szymonstefanski1@gmail.com>
2025-05-04 23:56:51 +0200

    Add endpoints in CarController and functions in CarRepository to return, add and update car

--- src/main/java/com/simpleproject/carfactory/CarController.java
+++ src/main/java/com/simpleproject/carfactory/CarController.java
@@ -1,24 +1,46 @@
 package com.simpleproject.carfactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/cars")
 public class CarController {
 
     @Autowired
     private CarRepository carRepository;
 
-    @GetMapping("/test")
-    public int test() {
-        return 1;
-    }
-
-    @GetMapping("/cars")
+    @GetMapping("")
     public List<Car> getAll() {
         return carRepository.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Car getById(@PathVariable int id) {
+        return carRepository.getById(id);
+    }
+
+    @PostMapping("")
+    public int add(@RequestBody List<Car> cars) {
+        return carRepository.add(cars);
+    }
+
+    @PutMapping("/{id}")
+    public int update(@PathVariable("id") int id, @RequestBody Car updatedCar) {
+        Car car = carRepository.getById(id);
+
+        if (car != null) {
+            car.setBrand(updatedCar.getBrand());
+            car.setModel(updatedCar.getModel());
+            car.setYear(updatedCar.getYear());
+
+            carRepository.update(car);
+
+            return 1;
+        } else {
+            return -1;
+        }
     }
 }
--- src/main/java/com/simpleproject/carfactory/CarRepository.java
+++ src/main/java/com/simpleproject/carfactory/CarRepository.java
@@ -17,4 +17,23 @@ public class CarRepository {
         return jdbcTemplate.query("SELECT * FROM cars",
                 BeanPropertyRowMapper.newInstance(Car.class));
     }
+
+    public Car getById(int id) {
+        return jdbcTemplate.queryForObject("SELECT * FROM cars WHERE " + "id = ?",
+                BeanPropertyRowMapper.newInstance(Car.class), id);
+    }
+
+    public int add(List<Car> cars) {
+        cars.forEach(car -> jdbcTemplate
+                .update("INSERT INTO cars(brand, model, year) VALUES (?,?,?)"
+                ,car.getBrand(),car.getModel(),car.getYear()
+                ));
+
+        return 1;
+    }
+
+    public int update(Car car) {
+        return jdbcTemplate.update("UPDATE cars SET brand=?, model=?, year=? WHERE id=?",
+                car.getBrand(),car.getModel(),car.getYear(),car.getId());
+    }
 }
